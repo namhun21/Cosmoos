70441a18dc60a00dfd261923ba7234017b837878
towsung0220 <towsung0220@naver.com>
2017-10-08 21:17:09 +0900

    Hi!

--- av.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-void main(){
-
-	printf("안녕");
-	printf("안녕하세요")
-	printf("반갑");
-}
--- hola.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main()
-{
-	printf("helwlllll");
-	printf("battle ground");
-	printf("harry");
-}
-
