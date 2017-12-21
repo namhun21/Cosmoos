8b23deeb609ea6971d1028a00667df8323e77201
namhun21 <namhun21@naver.com>
2017-12-21 16:37:16 +0900

    수정

--- PoC/Overlay/T-Shirt/Upper_Body(범위 나눈거).py
+++ PoC/Overlay/T-Shirt/Upper_Body(범위 나눈거).py
@@ -1,17 +1,22 @@
 import cv2
 import sys
 import numpy as np
+import cv2
+import os
+
+
+os.system('sudo modprobe bcm2835-v4l2')
 
 #cascPath = sys.argv[0]
-bodyCascade = cv2.CascadeClassifier('haarcascade_mcs_upperbody.xml')
-body_mask = cv2.imread('T-Shirt.png')
-h_mask, w_mask = body_mask.shape[:2]
+bodyCascade = cv2.CascadeClassifier('haarcascade_mcs_upperbody.xml') #학습데이터 읽어오기
+body_mask = cv2.imread('T-Shirt.png')#이미지 읽어오기
+h_mask, w_mask = body_mask.shape[:2] 
 
-if bodyCascade.empty():
+if bodyCascade.empty(): 
     raise IOError('Unable to load the mouth cascade classifier xml files')
 
-cap = cv2.VideoCapture(0)
-scaling_factor = 1.5
+cap = cv2.VideoCapture(0) #내장 카메라 
+scaling_factor = 1.5 #윈도우 크기설정
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -36,10 +41,18 @@ while True:
         mask_inv = cv2.bitwise_not(mask)
         print(body_mask.shape)
         print(body.shape)
-        masked_body = cv2.bitwise_and(body_mask_small,body_mask_small, mask = mask)
-        masked_frame = cv2.bitwise_and(frame_roi, frame_roi, mask=mask_inv)
-        frame[y+150:y+450,x:x+300] = cv2.add(masked_body, masked_frame)
-        cv2.rectangle(frame, (x, y+150), (x+300 ,y+450), (0, 255, 0), 2)
+        print(x)
+        print(y)
+        if(x<50 or x>660 or y>270):
+            continue
+        else:
+            masked_body = cv2.bitwise_and(body_mask_small,body_mask_small, mask = mask)
+        
+            masked_frame = cv2.bitwise_and(frame_roi, frame_roi, mask=mask_inv)
+        
+            frame[y+150:y+450,x:x+300] = cv2.add(masked_body, masked_frame)
+        
+            cv2.rectangle(frame, (x, y+150), (x+300 ,y+450), (0, 255, 0), 2)
 
     for (x, y, w, h) in body:
         #cv2.rectangle(frame, (x, y+400), (x+w, y+h+500), (0, 255, 0), 3)
