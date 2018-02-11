f890e1c0c325b9881e61324e88e3f3a3ba8a1880
kabsra <yrs01124@naver.com>
2018-02-11 13:49:42 +0900

    dong

--- project/start2/start2.py
+++ project/start2/start2.py
@@ -6,10 +6,11 @@ import os
 
 cap = cv2.VideoCapture(0)
 font                   = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText_Title = (0,20)
 bottomLeftCornerOfText1 = (460,70)
 bottomLeftCornerOfText2 = (260,70)
 bottomLeftCornerOfText3 = (60,70)
-fontScale              = 0.5
+fontScale              = 1
 fontColor              = (255,255,255)
 lineType               = 2
 
@@ -21,21 +22,28 @@ while True:
     #cv2.rectangle(frame, (50, 50), (170, 80), (255, 0, 0), 3)
     #cv2.circle(frame, (450,50), 40, (0,0,255), -1)
 
-    
+    #변수 a 에 start1에서 선택한 항목의 값 저장
+    a = 'T-shirt'
+    cv2.putText(frame,a, 
+        bottomLeftCornerOfText_Title, 
+        font, 
+        0.5,
+        fontColor,
+        lineType)  
 
-    cv2.putText(frame,'T-shirt', 
+    cv2.putText(frame,'Reco', 
         bottomLeftCornerOfText1, 
         font, 
         fontScale,
         fontColor,
         lineType)
-    cv2.putText(frame,'Y-shirt', 
+    cv2.putText(frame,'List', 
         bottomLeftCornerOfText2, 
         font, 
         fontScale,
         fontColor,
         lineType)
-    cv2.putText(frame,'Hood-T', 
+    cv2.putText(frame,'Back', 
         bottomLeftCornerOfText3, 
         font, 
         fontScale,
