a9f6f4ec6bd27f9b02e292ba36a51380f4c9b44d
Jung-Jaejin <ymc7737@naver.com>
2018-02-22 15:22:28 +0900

    clickfunction

--- project/click.py
+++ project/click.py
@@ -1,71 +1,76 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(0)
 
 
-count =0
-num = 0
-time = 0
-touch = 0
-check = 0
-kernel = np.ones((5,5),np.uint8)
-
-while(cap.isOpened()):
-    ret, img = cap.read()
-    img1 = img.copy()
-##    cv2.imshow('input',img)
-    roi = img1[0:100, 0:150]
-    cv2.rectangle(img,(0,0),(150,100),(255,0,0),3)
-    imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    roigray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
-##    ret, immaskori = cv2.threshold(imgray,180,0,cv2.THRESH_BINARY)
+def clicka(a,b,c,d):
+    cap = cv2.VideoCapture(0)
 
 
-    if(check == 0 and time > 100):
-        cap1 = cv2.VideoCapture(0)
-        ret, ori2 = cap1.read()
-        oriroi = ori2[0:100,0:150]
-        origray = cv2.cvtColor(ori2,cv2.COLOR_BGR2GRAY)
-        origraysc = origray[0:100, 0:150]
-        cv2.imshow('asdasdasdaq',origray)
-        check = 1
-
-
-    if(check == 1):
-        for x in range(150):
-            for y in range(100):
-              oricolor = roigray[y,x]
-              roicolor = origraysc[y,x]
-              if(oricolor- roicolor < 30):
-                   roi[y,x] = 0
-              else:
-                   roi[y,x] = 255
-                   num = num+1
-
-    print(num)
-    if(num > 15000 * 0.07 and count == 0 and time > 50 and touch < 30):
-        count = 1
-    if(count == 1 and num < 15000 * 0.1 and touch < 30):
-        count = 2
-    if(count == 2 and num > 15000 * 0.15 and touch < 30):
-        count = 3
-    if(count ==3 and num < 15000* 0.1 and touch < 30):
-        count = 0
-        print("success")
-
-    touch = touch + 1
-    if(touch == 31):
-        touch = 0
-        count = 0
+    count =0
     num = 0
-    print(count)
-##    erosion = cv2.erode(roi,kernel,iterations= 1)
-    opening = cv2.morphologyEx(roi,cv2.MORPH_OPEN,kernel)
-    cv2.imshow('gray',imgray)
-    cv2.imshow('asd',opening)
-    time  = time + 1
-    k = cv2.waitKey(10)
-    if k==27:
-        break
+    time = 0
+    touch = 0
+    check = 0
+    kernel = np.ones((5,5),np.uint8)
 
-cv2.destroyAllWindows()
+    while(cap.isOpened()):
+        ret, img = cap.read()
+        img1 = img.copy()
+    ##    cv2.imshow('input',img)
+        roi = img1[b:d,a:c]
+        cv2.rectangle(img,(a,b),(c,d),(255,0,0),3)
+        imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        roigray = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
+    ##    ret, immaskori = cv2.threshold(imgray,180,0,cv2.THRESH_BINARY)
+
+
+        if(check == 0 and time > 100):
+            cap1 = cv2.VideoCapture(0)
+            ret, ori2 = cap1.read()
+            oriroi = ori2[b:d,a:c]
+            origray = cv2.cvtColor(ori2,cv2.COLOR_BGR2GRAY)
+            origraysc = origray[b:d,a:c]
+            cv2.imshow('asdasdasdaq',origray)
+            check = 1
+
+
+        if(check == 1):
+            for x in range(c-a):
+                for y in range(d-b):
+                  oricolor = roigray[y,x]
+                  roicolor = origraysc[y,x]
+                  if(oricolor- roicolor < 30):
+                       roi[y,x] = 0
+                  else:
+                       roi[y,x] = 255
+                       num = num+1
+
+        print(num)
+        if(num > 15000 * 0.5 and count == 0 and time > 50 and touch < 30):
+            count = 1
+        if(count == 1 and num < 15000 * 0.3 and touch < 30):
+            count = 2
+        if(count == 2 and num > 15000 * 0.5 and touch < 30):
+            count = 3
+        if(count ==3 and num < 15000* 0.3 and touch < 30):
+            count = 0
+            return 1
+            break
+
+        touch = touch + 1
+        if(touch == 31):
+            touch = 0
+            count = 0
+        num = 0
+        print(count)
+    ##    erosion = cv2.erode(roi,kernel,iterations= 1)
+    ##    opening = cv2.morphologyEx(roi,cv2.MORPH_OPEN,kernel)
+        cv2.imshow('gray',imgray)
+    ##    cv2.imshow('asd',opening)
+        cv2.imshow('sads',roigray)
+        time  = time + 1
+        k = cv2.waitKey(10)
+        if k==27:
+            break
+
+    cv2.destroyAllWindows()
