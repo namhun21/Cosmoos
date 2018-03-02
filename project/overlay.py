d0c49839b306248d5c3bce7c6143a7720979a546
namhun21 <namhun21@naver.com>
2018-03-02 18:30:49 +0900

    overlay 주석추가

--- project/overlay.py
+++ project/overlay.py
@@ -25,10 +25,10 @@ def Capture_Frame():  # 비디오 설정
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # BGR-> Gray
     return frame, gray
 
-def masked_Operation(x,y,w,h,frame,body_mask):
+def masked_Operation(x,y,w,h,frame,body_mask): # 상체 ROI의 범위를 정하고 이미지를 해당 영역에 덮어씌운다
 
     x = x-40        
-    y_offset = 220
+    y_offset = 220        # 이미지 사이즈 조정
     img_size = w +45
 
     frame_roi = frame[y+y_offset:y+y_offset+img_size, x:x+img_size]
@@ -48,7 +48,7 @@ def masked_Operation(x,y,w,h,frame,body_mask):
     frame[y+y_offset:y+y_offset+img_size, x:x+img_size] = cv2.add(masked_body, masked_frame) # 화면에 이미지 오버레이
 
      
-def Range_Operation(body,frame,body_mask):
+def Range_Operation(body,frame,body_mask):    # 예외범위를 추가한 코드
     count = 0
     global prev_x
     global prev_y
@@ -59,11 +59,11 @@ def Range_Operation(body,frame,body_mask):
         
         #print('x = ',x,'y = ',y,'w = ',w,'h = ',h)
 
-        if (abs(prev_x-x) < 10) and (abs(prev_y-y) < 10): #이전 x,y 와 현재 x,y의 차이가 별로 나지 않으면 
+        if (abs(prev_x-x) < 10) and (abs(prev_y-y) < 10): #이전 x,y 와 현재 x,y의 차이가 별로 나지 않으면 이전 위치의 이미지 출력
          #   print('prev_x =', prev_x, 'x = ', x, 'prev_y = ',prev_y, 'y = ', y, 'w= ',prev_w, 'h = ',prev_h)
             continue
 
-        if (prev_w != 0):
+        if (prev_w != 0):             
             if (abs(prev_w-w) > 10) and (abs(prev_h-h) > 10):
           #      print('prev_w = ',prev_w,'w = ',w,'prev_h = ',prev_h,'h = ',h)
                 continue
@@ -77,21 +77,21 @@ def Range_Operation(body,frame,body_mask):
         prev_w = w
         prev_h = h
         
-        if  (x>120 and x<550 ):     #특정 영역을 ROI로
+        if  (x>120 and x<550 ):     # 특정 영역을 벗어나지 않으면 오버레이
             masked_Operation(x,y,w,h,frame,body_mask)
 
-        else:
+        else:         # 특정영역 벗어나면 continue
             continue
         
             
-    if (count == 0):
+    if (count == 0):   # 이동 전, 후 차이가 적으면 이전 오버레이위치 출력 
         if (prev_x !=0 and prev_y !=0 and prev_w != 0 and prev_h !=0):
             masked_Operation(prev_x,prev_y,prev_w,prev_h,frame,body_mask)
     
         #cv2.rectangle(frame,(prev_x,prev_y),(prev_x+prev_w,prev_y+prev_h),(0,255,255),2)
         #draw_shirt(x,y,w,h)
         
-def Draw_Rect(frame,position,y1,y2,name):
+def Draw_Rect(frame,position,y1,y2,name):  # 카테고리 출력을 위한 함수
     font= cv2.FONT_HERSHEY_SIMPLEX
     Position=position
     fontScale              = 1
@@ -106,7 +106,7 @@ def Draw_Rect(frame,position,y1,y2,name):
         fontColor,
         lineType)
     
-def Full_Overlay():       #i 번째 옷 오버레이하는 정의 함수
+def Full_Overlay():       #이전에 정의했던 함수들을 모아서 처리
     # clothes - 1 overlay 
     #os.system('sudo modprobe bcm2835-v4l2')
 
