4f8ec051aff737714576a8c4de3da67276dc4b5e
kabsra <yrs01124@naver.com>
2018-03-10 15:25:01 +0900

    delete origray

--- project/UI_Sub.py
+++ project/UI_Sub.py
@@ -60,11 +60,11 @@ def Second_Menu(title,cap):
             # oriroi2 = ori2[50:80,250:370]
             # oriroi3 = ori2[50:80,50:170]
 
-            origray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            #origray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            origraysc1 = Click_Function.make_Roi(origray, 450, 570)
-            origraysc2 = Click_Function.make_Roi(origray, 250, 370)
-            origraysc3 = Click_Function.make_Roi(origray, 50, 170)
+            origraysc1 = Click_Function.make_Roi(gray, 450, 570)
+            origraysc2 = Click_Function.make_Roi(gray, 250, 370)
+            origraysc3 = Click_Function.make_Roi(gray, 50, 170)
 
             origraysc = [origraysc1, origraysc2, origraysc3]
 
