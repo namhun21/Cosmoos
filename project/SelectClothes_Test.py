5cec01888ab4c8dfb0381bcc2931dcfac369a5b2
Jung-Jaejin <ymc7737@naver.com>
2018-03-12 14:35:09 +0900

    I chaned little

--- project/SelectClothes_Test.py
+++ project/SelectClothes_Test.py
@@ -8,10 +8,9 @@ import overlay
 def SelectClothes(cap):
     TextLocation1=(50, 230)
     TextLocation2=(550, 230)
+    TextLocation3=(310, 430)
     #cap = cv2.VideoCapture(0)
     index = 0 #옷의 배열 인덱스
-    leftcount = 0#손이 올렸을 때 바로 클릭인지되지 않도록 20됐을 때 동작 실행
-    rightcount = 0#손이 올렸을 때 바로 클릭인지되지 않도록 20됐을 때 동작 실행
     overlaycount = 0#손이 올렸을 때 바로 클릭인지되지 않도록 20됐을 때 동작 실행
     LeftOn = 0#애니메이션 왼쪽 오른쪽 구분하기 위하여 선언
     RightOn = 0#애니메이션 왼쪽 오른쪽 구분하기 위하여 선언
@@ -30,24 +29,25 @@ def SelectClothes(cap):
     while(cap.isOpened()):
         ret, img = cap.read()
         img1 = cv2.flip(img,1)
-        
-        
+
+
         Click_Function.draw_Click(img1,TextLocation1,(10,200),(140,250),"Turn Left")
         Click_Function.draw_Click(img1,TextLocation2,(510,200),(640,250),"Turn Right")
+        Click_Function.draw_Click(img1,TextLocation3,(280,400),(410,450),"Overlay")
         #cv2.rectangle(img1,(10,200),(180,250),(255,0,0),3)
         #cv2.rectangle(img1,(200,350),(350,650),(255,0,0),3)
         #cv2.rectangle(img1,(510,250),(640,300),(255,0,0),3)
-        
+
         imgray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
         leftButtonFrame = imgray[200:250,10:140]
         rightButtonFrame = imgray[200:250,510:640]
-        #overlayButtonFrame = imgray[350:450,200:350]
+        overlayButtonFrame = imgray[400:450,280:410]
 
         if(startcompare == 0 and timeright > 100 and timeLeft > 100):#루프를 100번 돌고나서 비교할 이미지 추출
             picturegray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
             pictureleftButtonFrame = picturegray[200:250,10:140]#왼쪽 애니메이션 버튼 부분
             picturerightButtonFrame = picturegray[200:250,510:640]#오른쪽 애니메이션  버튼 부분
-            #pictureoverlayButtonFrame = picturegray[350:450,200:350]#가운데 overlay 버튼 부분
+            pictureoverlayButtonFrame = picturegray[400:450,280:410]#가운데 overlay 버튼 부분
             startcompare = 1 #비교 시작
 
         if(startcompare == 1):#비교하기 위한 이미지 추출
@@ -72,48 +72,35 @@ def SelectClothes(cap):
                        whiteNumLeft = whiteNumLeft+1#왼쪽 부분의 흰색 픽셀 개수 증가
 
 
-            #for x in range(150):
-             #   for y in range(100):
-              #    picturecolor = overlayButtonFrame[y,x]#현재 프레임의 가운데 버튼 부분
-               #   overlayButtonColor = pictureoverlayButtonFrame[y,x]#찍어놓은 이미지의 가운데 버튼 부분
-                #  if(picturecolor- overlayButtonColor < 30):#비교해서 색의 차가 30 미만일 때
-                 #      overlayButtonFrame[y,x] = 0#흑색으로 변환
-                  #else:
-                   #    overlayButtonFrame[y,x] = 255#백색으로 변환
-                    #   whiteNumOverlay = whiteNumOverlay+1#가운데 부분의 흰색 픽셀 개수 증가
+            for x in range(130):
+                for y in range(50):
+                  picturecolor = overlayButtonFrame[y,x]#현재 프레임의 가운데 버튼 부분
+                  overlayButtonColor = pictureoverlayButtonFrame[y,x]#찍어놓은 이미지의 가운데 버튼 부분
+                  if(picturecolor- overlayButtonColor < 30):#비교해서 색의 차가 30 미만일 때
+                       overlayButtonFrame[y,x] = 0#흑색으로 변환
+                  else:
+                       overlayButtonFrame[y,x] = 255#백색으로 변환
+                       whiteNumOverlay = whiteNumOverlay+1#가운데 부분의 흰색 픽셀 개수 증가
 
 
-        if(whiteNumRight > 15000*0.8):
-            rightcount = rightcount + 1
-        else:#손을 버튼에서 떼면 초기화가 되서 다시 카운트를 시작해야한다.
-            rightcount = 0
-
-        if(rightcount == 20):#애니메이션 왼쪽 모드 가동
+        if(whiteNumRight > 6500 *0.7):
             LeftOn = 0
             RightOn = 1
             move = 1
 
+        if(whiteNumLeft > 6500*0.7):
+           LeftOn = 1
+           RightOn = 0
+           move = 1
 
+        if(whiteNumOverlay > 6500 *0.7):
+            overlaycount = overlaycount + 1
+        else:
+            overlaycount = 0
 
-        if(whiteNumLeft > 15000*0.8):
-            leftcount = leftcount + 1
-        else:#손을 버튼에서 떼면 초기화가 되서 다시 카운트를 시작해야한다.
-            leftcount = 0
-
-        if(leftcount == 20):#애니메이션 왼쪽 모드 가동
-            LeftOn = 1
-            RightOn = 0
-            move = 1
-
-
-      #  if(whiteNumOverlay > 15000 *0.8):
-         #   overlaycount = overlaycount + 1
-       # else:
-        #    overlaycount = 0
-
-        #if(overlaycount == 20):#오버레이 창으로 전환
-         #   overlaycount = 0
-          #  overlay.Full_Overlay()
+        if(overlaycount == 20):#오버레이 창으로 전환
+            overlaycount = 0
+            overlay.Full_Overlay()
 
 
 
@@ -122,7 +109,7 @@ def SelectClothes(cap):
 
         if(index == 0):#가운데 이미지의 배열 인덱스가 0일 시의 예외 처리
             if(RightOn == 1):
-                  AnimationRightCall.animationright(index+3,index+4,index,index+1,animationUnit,img1)
+                AnimationRightCall.animationright(index+3,index+4,index,index+1,animationUnit,img1)
             else:
                 AnimationLeftCall.animationleft(index+4,index,index+1,index+2,animationUnit,img1)
         elif(index == 4):#가운데 이미지의 배열 인덱스가 4일 시의 예외 처리
@@ -160,11 +147,11 @@ def SelectClothes(cap):
 
         timeright = timeright + 1#비교할 프레임을 찍기위한 시간 체크변수 1증가(100때 이미지 비교 시작)
         timeLeft = timeLeft + 1#비교할 프레임을 찍기위한 시간 체크변수 1증가(100때 이미지 비교 시작)
-        print("leftasdasdsa",LeftOn)
-        print("rightasdasdsa",RightOn)
+        print(whiteNumLeft)
+        print(whiteNumRight)
         cv2.imshow('wqzxcq',rightButtonFrame)
         cv2.imshow('funsadasd',leftButtonFrame)
-        #cv2.imshow('sjdwnmanmd',overlayButtonFrame)
+        cv2.imshow('sjdwnmanmd',overlayButtonFrame)
         cv2.imshow('asdasda',img1)
         whiteNumRight = 0#흰색 픽셀의 개수 초기화
         whiteNumLeft = 0#흰색 픽셀의 개수 초기화
